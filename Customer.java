17edac714dfa6b3c80d396b195a2df688e53bc8f
RicardoAcostaArambula <acostaricardo463@gmail.com>
2024-10-15 12:23:18 -0600

    adds customer class that extends person, adds attributes, constructors, setters and getters

---
+++ Customer.java
@@ -0,0 +1,33 @@
+import java.util.List;
+public class Customer extends Person {
+    
+    /*Attributes*/
+    private String customer_id;
+    private List<String> accounts;
+    /*constructors*/
+    /*default constructor */
+    public Customer(){
+        
+    }
+    public Customer(String customer_id){
+        this.customer_id = customer_id;
+    }
+    public Customer(String customer_id, List<String> accounts){
+        this.customer_id = customer_id;
+        this.accounts = accounts;
+    }
+    /*setters*/
+    public void set_account_id(String customer_id){
+        this.customer_id = customer_id;
+    }
+    public void set_accounts(List<String> accounts){
+        this.accounts = accounts ;
+    }
+    /*getters*/
+    public String set_account_id(){
+        return this.customer_id;
+    }
+    public List<String>  set_accounts(){
+        return this.accounts;
+    }
+}
